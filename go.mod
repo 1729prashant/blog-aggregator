325c221108daa49f1e668c7f0da1e862dd6bf3dd
Prashant V Gaikar <1729prashant@gmail.com>
2024-12-04 09:31:52 +0530

    created internals

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/1729prashant/blog-aggregator
+
+go 1.23.3
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+import (
+	"internal/config"
+)
+*/
+
+func main() {
+	//Read the config file.
+
+	//Set the current user to "lane" (actually, you should use your name instead) and update the config file on disk.
+
+	//Read the config file again and print the contents of the config struct to the terminal.
+}
